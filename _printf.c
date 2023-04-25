199ea62e46cd6102e452919a3607fb56bd3c242e
Guilian Asongtia <asonguillian@gmail.com>
2023-04-25 11:53:46 +0000

    Adds fix for invalid format specifier

--- _printf.c
+++ _printf.c
@@ -42,53 +42,7 @@ int print_percent(va_list *args __attribute__((unused)))
 	write(1, &c, 1);
 	return (1);
 }
-/**
- * print_int - prints an integer from a va_list argument
- * @args: va_list argument from which integer is read
- * Return: number of characters printed
- */
-int print_int(va_list *args)
-{
-	int val = va_arg(*args, int);
-	char buffer[20];/*max num of digits for 64 bits int +sign */
-	int i = 0; /*index of next character to write to buffer*/
-	int negative = 0; /* flag to denote sign of the integer*/
-	int count = 0;
 
-	if (val < 0)
-	{
-		negative = 1;
-		if (val == INT_MIN)/*handle integr overflow*/
-		{
-			write(1, "-2147483648", 11);
-			return (11);
-		}
-		val = -val;
-	}
-	/* check for integer overflow */
-	if (val >= 1000000000 && (val > INT_MAX / 1000000000
-				|| val * 1000000000 > INT_MAX
-				- (val % 1000000000) * 2))
-	{
-		write(1, "OVERFLOW\n", 9);
-		return (9);
-	}
-	/* if integer is within limits, convert to string in reverse order */
-	do {
-		buffer[i++] = val % 10 + '0';
-		val /= 10;
-	} while (val > 0);
-	if (negative)/*append sign if integer is -ve*/
-	{
-		buffer[i++] = '-';
-	}
-	while (i > 0)/* write string in reverse order*/
-	{
-		write(1, &buffer[--i], 1);
-		++count;
-	}
-	return (count);
-}
 /**
  * _printf - produces output according to a foramt
  * @format: character string that contains directive for printing
@@ -118,10 +72,12 @@ int _printf(const char *format, ...)
 				case '%':
 					print_func = &print_percent;
 					break;
-				case 'd':
-				case 'i':
-					print_func = &print_int;
-					break;
+				default:
+					write(1, "%", 1);
+					write(1, format, 1);
+					count += 2;
+					++format;
+					continue;
 			}
 			++format;
 			count += print_func(&args);
